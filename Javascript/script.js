80724e0f6ca07a84a6691a5070f775b4204727f3
x3n0N69 <abhi973686@gmail.com>
2022-07-02 18:36:03 +0530

    Learning1_3

--- Javascript/script.js
+++ Javascript/script.js
@@ -14,3 +14,23 @@ function toCelsius() {
   let resultCelsius = (5 / 9) * (fahrenheit - 32);
   document.getElementById("celcius-result").innerHTML = resultCelsius;
 }
+
+// classes:
+
+class car {
+  constructor(name, year) {
+    this.name = name;
+    this.year = year;
+  }
+
+  age() {
+    let d = new Date();
+    d = d.getFullYear();
+    return d - this.year;
+  }
+}
+
+let myCar = new car("Indica Vista", 2010);
+document.getElementById("car-age").innerHTML = `Car: ${
+  myCar.name
+}, car's age: ${myCar.age()}`;
